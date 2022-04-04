300181a31b6f0381c635994313f810bb0b27cc6a
Mhaemnn <ismuhaimin1@gmail.com>
2022-04-04 22:48:53 +0700

    modifikasi

--- #03 Casting Tipe Data/main.py
+++ #03 Casting Tipe Data/main.py
@@ -1,55 +1,33 @@
-print("===BELAJAR CASTING TIPE DATA====")
-# belajar casting tipe data: merubah dati satu data kedata lain
-# tipe data: int, float, string, boolean
+import string
+from xmlrpc.client import boolean
 
 
-# INTEGER 
-print("===========INTEGER==========")
-data_int = 9
-print("data=", data_int, "type=", type(data_int))
-
-data_float = float(data_int)
-data_str = str(data_int)
-data_bool = bool(data_int)  # akan flase jika nilai int=0
-print("data=", data_float, "type=", type(data_float))
-print("data=", data_str, "type=", type(data_str))
-print("data=", data_bool, "type=", type(data_bool))
+print("============BELAJAR CASTING TIPE DATA=============")
+#casting adalah merubah dari satu tipe data ke tipe data lain
+#TIPE DATA : INT, FLOAT, STR, BOOLEAN
 
 
-# FLOAT
-print("===========FLOAT==========")
-data_float = 9.7
-print("data=", data_float, "type=", type(data_float))
+#integer
+print("==================================================")
+data_int     = 34
+data_float   = float(data_int)
+data_str     = str(data_int)
+data_boolean = bool(data_int)
 
-data_int = int(data_float)  # akan di bulatkan ke bawah
-data_str = str(data_float)
-data_bool = bool(data_float)  # akan false jika nilai float bernilai = 0
-print("data=", data_int, "type=", type(data_int))
-print("data=", data_str, "type=", type(data_str))
-print("data=", data_bool, "type=", type(data_bool))
-
-
-# STRING
-print("===========STRING==========")
-data_str = "23"
-print("data=", data_str, "type=", type(data_str))
-
-data_int = int(data_str)  # string harus angka
-data_float = float(data_str)  # string harus anka
-data_bool = bool(data_str)  # akan false jika string kosong
-print("data=", data_int, "type=", type(data_int))
-print("data=", data_float, "type=", type(data_float))
-print("data=", data_bool, "type=", type(data_bool))
+print("data = ", data_int,"type   = ", type(data_int))
+print("data = ", data_float,"type = ", type(data_int))
+print("data = ", data_str,"type   = ", type(data_int))
+print("data = ", data_int,"type   = ", type(data_int))
 
 
-# BOOLEAN
-print("===========BOOLEAN==========")
-data_boolean = True
-print("data=", data_boolean, "type=", type(data_boolean))
+#float
+print("==================================================")
+data_float = 9.3
+data_int   = int(data_float)
+data_str   = int(data_float)
+data_bool  = int(data_float)
 
-data_int = int(data_boolean)
-data_str = str(data_boolean)
-data_float = float(data_boolean)  # akan flase jika nilai int=0
-print("data=", data_int, "type=", type(data_int))
-print("data=", data_str, "type=", type(data_str))
-print("data=", data_float, "type=", type(data_float))
+print("data = ", data_float, "type   = ", type(data_float))
+print("data = ", data_int, "type     = ", type(data_float))
+print("data = ", data_str, "type     = ", type(data_float))
+print("data = ", data_bool,"type     = ", type(data_float))
