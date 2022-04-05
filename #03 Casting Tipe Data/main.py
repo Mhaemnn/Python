3d838a4d37ed0ea7fd6b4e9d7326bd6505e808d0
Mhaemnn <ismuhaimin1@gmail.com>
2022-04-05 11:48:48 +0700

    modifikasi file

--- #03 Casting Tipe Data/main.py
+++ #03 Casting Tipe Data/main.py
@@ -1,6 +1,3 @@
-import string
-from xmlrpc.client import boolean
-
 
 print("============BELAJAR CASTING TIPE DATA=============")
 #casting adalah merubah dari satu tipe data ke tipe data lain
@@ -8,26 +5,51 @@ print("============BELAJAR CASTING TIPE DATA=============")
 
 
 #integer
-print("==================================================")
+print("======================integer======================")
 data_int     = 34
 data_float   = float(data_int)
 data_str     = str(data_int)
-data_boolean = bool(data_int)
+data_bool    = bool(data_int)
 
 print("data = ", data_int,"type   = ", type(data_int))
-print("data = ", data_float,"type = ", type(data_int))
-print("data = ", data_str,"type   = ", type(data_int))
-print("data = ", data_int,"type   = ", type(data_int))
+print("data = ", data_float,"type = ", type(data_float))
+print("data = ", data_str,"type   = ", type(data_str))
+print("data = ", data_int,"type   = ", type(data_bool))
 
 
 #float
-print("==================================================")
-data_float = 9.3
+print("======================float========================")
+data_float = 9.5
 data_int   = int(data_float)
-data_str   = int(data_float)
-data_bool  = int(data_float)
+data_str   = str(data_float)
+data_bool  = bool(data_float)
 
-print("data = ", data_float, "type   = ", type(data_float))
-print("data = ", data_int, "type     = ", type(data_float))
-print("data = ", data_str, "type     = ", type(data_float))
-print("data = ", data_bool,"type     = ", type(data_float))
+print("data = ", data_float,"type  = ", type(data_float))
+print("data = ", data_int,"type    = ", type(data_int))
+print("data = ", data_str,"type  = ", type(data_str))
+print("data = ", data_bool,"type = ", type(data_bool))
+
+
+#boolean
+print("======================boolean=======================")
+data_bool = True
+data_int   = int(data_bool)
+data_str   = str(data_bool)
+data_float = float(data_bool)
+
+print("data = ", data_bool, "type = ", type(data_bool))
+print("data = ", data_int, "type    = ", type(data_int))
+print("data = ", data_str, "type = ", type(data_str))
+print("data = ", data_float, "type  = ", type(data_float))
+
+#string
+print("======================string=========================")
+data_str   = "29";
+data_int   = int(data_str) #string harus angka
+data_float = float(data_str) #string harus angka
+data_bool  = bool(data_str) #false jika string kosong
+
+print("data = ", data_str, "type   = ", type(data_str))
+print("data = ", data_int, "type   = ", type(data_int))
+print("data = ", data_float, "type = ", type(data_float))
+print("data = ", data_bool, "type = ", type(data_bool))
