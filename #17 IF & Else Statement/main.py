39ac6ffd3c32b168b2dace32ebffa94259475091
Mhaemnn <ismuhaimin1@gmail.com>
2022-04-18 23:14:10 +0700

    modifikasi

--- #17 IF & Else Statement/main.py
+++ #17 IF & Else Statement/main.py
@@ -3,29 +3,31 @@
 # 1. if nya
 # 2. kondisinya
 # 3. aksinya
-
-nama = input("Siapa nama anda? ")
+print(20*"="+"IF & ELSE"+20*"=")
+nama = input("Siapa nama anda\t? ")
 
 # program sebelumnya
 # if kondisi: aksi
 # program selanjutnya
 
 # 1. program if inline
-# if nama=="ucup" : print("Kamu Ganteng abieeez!!!!")
+# if nama=="Muhaemin" : print("Kamu Ganteng abieeez!!!!")
 # print(f"Terima kasih {nama}")
 
 # 2. program if indentation
 
-# if nama=="ucup":
+# if nama=="Muhaemin":
 # 	print("kamu ganteng abieeeez!")
 # 	print("kamu juga keren banget!")
 # print(f"Terima kasih {nama}")
 
 # 3. Else Statement
 
-if nama == "otong":
-    print("hai otooong, si keren!!!")
+if nama == "Muhaemin":
+    print("Hai muhaemin, si anak kerennn!!!")
 else:
-    print("Ah kamu bukan otong, kamu gak keren! :(")
+    print("Ah Kamu bukan muhaemin, kamu gak keren! :(")
 
 print("akhir dari program")
+
+print(20*"="+"Contoh Lain"+20*"=")
