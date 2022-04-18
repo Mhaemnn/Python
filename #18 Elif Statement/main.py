b0506fc14d5e6553ace636af1eed60f92d30fe5c
Mhaemnn <ismuhaimin1@gmail.com>
2022-04-18 23:37:35 +0700

    modifikasi

--- #18 Elif Statement/main.py
+++ #18 Elif Statement/main.py
@@ -1,14 +1,18 @@
 # ELIF = else if statement
 
-nama = input("Nama kamyu siapa? ")
 
-if nama == "ucup":  # kondisi 1
-    print("Hai ganteeeeng beuds!")  # aksi true 1
-elif nama == "otong":  # kondisi 2
-    print("Hai si kece bangeeeets!!")  # aksi true 2
-elif nama == "mario":  # kondisi 3
-    print("Hai humooooreeeesh!")  # aksi true 3
+print(20*"="+"If and Elif"+20*"=")
+nama = input("Masukan Nama Cewe: \t")
+
+if nama == "Dewi Adennin": # Kondisi 1
+    print("Hai, aku sangat ingin memilikimu") # aksi true 1
+elif nama == "Riffa Mardiyyah":
+    print("Wanita yang cukup di kenal saja") # aksi true 2
+elif nama == "Novi Rindiani":
+    print("Orang bodoh yang menyia-menyiakan wkwk") # aksi true 3
 else:
-    print("au ah gak kenal!!!")  # aksi false
+    print("Dah lah cinta itu gak selamanya indah") # aksi false
+print("Akhir dari program")
 
-print("ini adalah akhir dari program")
+
+print(20*"="+"If and Elif"+20*"=")
