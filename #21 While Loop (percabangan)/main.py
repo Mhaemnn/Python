7f7118d2d31926e7f806271b28175ae00b1680d8
Mhaemnn <ismuhaimin1@gmail.com>
2022-04-19 13:46:17 +0700

    modifikasi

--- #21 While Loop (percabangan)/main.py
+++ #21 While Loop (percabangan)/main.py
@@ -5,13 +5,14 @@
 # 	aksi itu
 
 # akhir dari program
-
+print(20*"="+"\n")
 angka = 0
 print(f"angka sekarang -> {angka}")
 
 while angka < 5:
-    angka += 1
+    angka += 2
     print(f"angka sekarang -> {angka}")
-    print("otong ganteng maxsyimaal!")
+    print(f"ditambahkan dengan 1")
 
-print("cukuuup")
+print("akhir dari program")
+print("\n"+20*"=")
