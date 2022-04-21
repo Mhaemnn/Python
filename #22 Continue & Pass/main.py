dcf829851836d51ef27bb19c1ba209b488a4d360
Mhaemnn <ismuhaimin1@gmail.com>
2022-04-21 23:51:34 +0700

    modifikasi

--- #22 Continue & Pass/main.py
+++ #22 Continue & Pass/main.py
@@ -14,15 +14,18 @@
 
 # continue
 
-angka = 0
 
-print(f"angka sekarang -> {angka}")
 
-while angka < 5:
-    angka += 1
-    print(f"angka sekarang -> {angka}")  # aksi 1
 
-    if angka == 3:
+ankga = 10
+
+print(f"ankga sekarang -> {ankga}")
+
+while ankga < 5:
+    ankga += 1
+    print(f"ankga sekarang -> {ankga}")  # aksi 1
+
+    if ankga == 3:
         print("nice!")
         continue  # akan membuat loop meloncat ke step selanjutnya
 
