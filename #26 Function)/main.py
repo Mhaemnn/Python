ddd405d76d260e3647fa827d39e59b2c07742dd3
Mhaemnn <ismuhaimin1@gmail.com>
2022-04-24 13:16:45 +0700

    :modifikasi

--- #26 Function)/main.py
+++ #26 Function)/main.py
@@ -1,24 +1,47 @@
-# membuat fungsi sederhana
-def suaraayam():
-    print('kukuruyuk!!!!')
+print("==================//////====================")
+def fungsi_total_nilai(var_harian, var_uts, var_uas) :
+    var_harian = int(var_harian) * 0.3
+    var_uts = int(var_uts) * 0.3
+    var_uas = int(var_uas) * 0.4
+
+    var_total = var_harian + var_uts + var_uas
+    return var_total
+
+#Deklarasi Fungsi Percabangan
+def fungsi_percabangan (var_nilai) :
+    var_huruf = ""
+    if (var_nilai >= 0 and var_nilai < 20) :
+        var_huruf = "E"
+    elif (var_nilai >= 20 and var_nilai < 40) :
+        var_huruf = "D"
+    elif (var_nilai >= 40 and var_nilai < 60) :
+        var_huruf = "C"
+    elif (var_nilai >= 60 and var_nilai < 80) :
+        var_huruf = "B"
+    elif (var_nilai >= 80 and var_nilai < 100) :
+        var_huruf = "A"
+    return var_huruf
+
+#Deklarasi Fungsi Perulangan
+def fungsi_perulangan():
+    var_hasil_perulangan = 0
+    for i in range(1,3):
+        print("--------Nilai Ke ",i,"--------")
+        var_harian = input("Nilai Harian \t: ")
+        var_uts = input("Nilai UTS \t: ")
+        var_uas = input("Nilai UAS \t: ")
+
+        #Pemanggilan fungsi Penjumlahan
+        var_hasil_perulangan +=(int(fungsi_total_nilai(var_harian, var_uts, var_uas)))
+
+    return var_hasil_perulangan /i
 
 
-def hargaayam():
-    suaraayam()
-    print('harga ayam per 1 kg adalah Rp. 20.000')
+#Pemanggilan fungsi perulangan
+var_total = fungsi_perulangan()
 
-# membuat fungsi dengan input argumen
+print("--------Total Nilai --------")
+print("Total nilai yang didapat \t: ",var_total)
 
-
-def hargatotalayam(kg):
-    suaraayam()
-    harga = 20000
-    hargaTotal = kg*harga
-    print('harga', kg, 'kg ayam adalah', hargaTotal)
-
-
-hargaayam()
-hargatotalayam(2)
-hargatotalayam(0.5)
-hargatotalayam(1)
-hargatotalayam(9)
+#Pemanggilan Fungsi Percabangan
+print("Total Nilai Dalam Huruf \t: ", fungsi_percabangan(var_total))
