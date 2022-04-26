9cc6c8217414edc94770021f627e9bc2944da914
Mhaemnn <ismuhaimin1@gmail.com>
2022-04-26 10:24:51 +0700

    modifikasi

--- #35 Dictionary/latihan_dictionary.py
+++ #35 Dictionary/latihan_dictionary.py
@@ -1,16 +1,7 @@
-# dictionary
-
-member1 = {"ID": 101,
-           "Nama": "Faqihza Mukhlish",
-           "Pekerjaan": "Mahasiswa",
-           "Status member": "Gold"
-           }
-
-member2 = {"ID": 102,
-           "Nama": "Ujang Pintu",
-           "Pekerjaan": "reparasi pintu",
-           "Status member": "Berlian"}
-
-memberlist = {101: member1, 102: member2}
-
-print(memberlist[101])
+buku = {
+  "judul": "remaja bodoh",
+  "penulis": "tahu bulat",
+  "tanggal": "26/04/2022",
+  "katagory": "manusia bodoh"
+}
+print(buku)
