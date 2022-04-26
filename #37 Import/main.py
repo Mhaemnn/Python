f7d47d4087a007007b1d6d3e663dfb71ce1fd8f6
Mhaemnn <ismuhaimin1@gmail.com>
2022-04-26 23:17:27 +0700

    modifikasi

--- #37 Import/main.py
+++ #37 Import/main.py
@@ -1,5 +1,8 @@
 import modul
 
-print(modul.data)
+modul.tambah(89,1)
+modul.pengurangan(100,2)
+modul.perkalian(5,5)
+modul.pembagian(100,5)
 
-modul.cek_modul()
+print(modul)
--- #37 Import/modul.py
+++ #37 Import/modul.py
@@ -1,8 +1,22 @@
-data = "apa kabs?"
+"""
+# Belajar import
+    1. modul.py (penjumlahan)
+    2. main.py 
+"""
+print(20*"="+"Belajar Import"+20*"="+"\n")
 
+# Operasi Matematika
 
-def cek_modul():
-    print("hallo gan")
+def tambah(bil1, bil2):
+    hasil = bil1 + bil2
+    print("hasil tambah dari\t: ",bil1, "\t","+", bil2, "=", hasil)
 
-
-print("ini adalah modul saya")
+def pengurangan(bil1,bil2):
+    hasil = bil1 - bil2
+    print("hasil dari pengurangan\t: ", bil1,"\t", "-", bil2, "=", hasil)
+def perkalian(bil1, bil2):
+    hasil = bil1 * bil2
+    print("hasil dari perkalian\t: ", bil1, "\t","*", bil2, "=", hasil)
+def pembagian(bil1, bil2):
+    hasil = bil1 / bil2
+    print("hasil dari pembagian\t: ", bil1, "\t","/", bil2, "=", hasil)
