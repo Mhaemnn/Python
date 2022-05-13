0b60c0ff39170c57d1e534c889ebd2319d1c5607
Mhaemnn <ismuhaimin1@gmail.com>
2022-05-13 20:12:16 +0700

    modifikasi

--- #41 Pengenalan Class/main.py
+++ #41 Pengenalan Class/main.py
@@ -5,17 +5,17 @@ class Mahasiswa():
     jk = 'laki'
 
 
-muhaemin= Mahasiswa()
-muhaemin.nama = 'juminten'
-muhaemin.umur = 24
-muhaemin.status = 'single'
-muhaemin.jk = 'laki-laki'
+mahasiswa_1= Mahasiswa()
+mahasiswa_1.nama = 'Fahreza'
+mahasiswa_1.umur = 24
+mahasiswa_1.status = 'single'
+mahasiswa_1.jk = 'laki-laki'
 
-kamu = Mahasiswa()
-kamu.nama = 'markonah'
-kamu.umur = 50
-kamu.status = 'janda'
-kamu.jk = 'perempuan'
+mahasiswa_2 = Mahasiswa()
+mahasiswa_2.nama = 'Maemunah'
+mahasiswa_2.umur = 50
+mahasiswa_2.status = 'janda'
+mahasiswa_2.jk = 'perempuan'
 
-print('mahasiswa 1',muhaemin.nama, muhaemin.umur, muhaemin.status, muhaemin.jk)
-print('mahasiswa 2',kamu.nama, kamu.umur, kamu.status, kamu.jk)
+print('mahasiswa 1',mahasiswa_1.nama, mahasiswa_1.umur, mahasiswa_1.status, mahasiswa_1.jk)
+print('mahasiswa 2',mahasiswa_2.nama, mahasiswa_2.umur, mahasiswa_2.status, mahasiswa_2.jk)
