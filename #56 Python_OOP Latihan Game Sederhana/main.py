c878e15913b665085bd2ef164a2fc89c26ac7f48
Mhaemnn <ismuhaimin1@gmail.com>
2022-05-16 11:29:07 +0700

    modifikasi

--- #56 Python_OOP Latihan Game Sederhana/main.py
+++ #56 Python_OOP Latihan Game Sederhana/main.py
@@ -1,28 +1,15 @@
 print(10 * "=" + "Membuat Game Sederhana" + 10 * "="+"\n"+"\n")
-
 class Hero:
 
-    def __init__(self, name, health, attackPower, armorNumber):
-        self.name = name
+    def __init__(self,name, health, attackPower, armorPower) -> None:
+        self.name   = name
         self.health = health
-        self.attackPower = attackPower
-        self.armorNumber = armorNumber
+        self.attack = attackPower
+        self.armor  = armorPower
 
-    def serang(self, Hero):
-        print(self.name + ' menyerang ' + Hero.name)
-        Hero.diserang(self, self.attackPower)
+    def serang(self, lawan):
+        print(self.name, " menyerang ", lawan.name)
+        lawan.diserang(self, self.attackPower)
 
-    def diserang(self, Hero, attackPower_Hero):
-        print(self.name + ' diserang ' + Hero.name)
-        attack_diterima = attackPower_Hero/self.armorNumber
-        print('serangan terasa : ' + str(attack_diterima))
-        self.health -= attack_diterima
-        print('darah ' + self.name + ' tersisa ' + str(self.health))
-
-
-nanang = Hero('nanang', 100, 10, 5)
-nartoo = Hero('nartoo', 100, 20, 10)
-
-nanang.serang(nartoo)
-print("\n")
-nartoo.serang(nanang)
+    def diserang(self,lawan,attackPower_lawan):
+        print(self.name, " diserang ", lawan.name)
