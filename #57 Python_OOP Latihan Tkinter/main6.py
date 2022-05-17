48167d4b05a30e03ccdfe75832bf5f2b71a3dc18
Mhaemnn <ismuhaimin1@gmail.com>
2022-05-17 23:05:34 +0700

    modifikasi

--- #57 Python_OOP Latihan Tkinter/main6.py
+++ #57 Python_OOP Latihan Tkinter/main6.py
@@ -1,40 +1,38 @@
-from textwrap import fill
 from tkinter import Tk, Frame, Checkbutton, BOTH
 
-class Contoh(Frame):
-    def __init__(self,parent):
-      Frame.__init__(self,parent)
-      self.parent = parent
+class Example(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.parent = parent
 
-      self.initUI()
-      self.BuatChackButton()
+        self.initUI()
+        self.buatCheckButton()
 
-      def initUI(self):
+    def initUI(self):
         self.parent.title("Checkbutton")
         self.pack(fill=BOTH, expand=True)
         self.parent.geometry("250x150")
 
-      def BuatCheckButton(self):
-        cb = Checkbutton(self, text='ini checkbox 1')
+    def buatCheckButton(self):
+        cb = Checkbutton(self, text="ini checkbox 1")
         cb.place(x=50, y=50)
 
-        cb = Checkbutton(self, text='ini checkbox 2')
-        cb.place(x=50, y=50)
-        
-        cb = Checkbutton(self, text='ini checkbox 3')
-        cb.place(x=50, y=50)
-        
-        cb = Checkbutton(self, text='ini checkbox 4')
-        cb.place(x=50, y=50)
+        cb = Checkbutton(self, text="ini checkbox 2")
+        cb.place(x=50, y=70)
 
-        cb = Checkbutton(self, text='ini checkbox 5')
-        cb.place(x=50, y=50)
+        cb = Checkbutton(self, text="ini checkbox 3")
+        cb.place(x=50, y=90)
 
-        cb = Checkbutton(self, text='ini checkbox 6')
-        cb.place(x=50, y=50)
+        cb = Checkbutton(self, text="ini checkbox 4")
+        cb.place(x=50, y=110)
 
+        cb = Checkbutton(self, text="ini checkbox 4")
+        cb.place(x=50, y=130)
 
-if __name__ == "__init__":
-  root = Tk()
-  app  = Contoh(root)
-  root.mainloop()
+        cb = Checkbutton(self, text="ini checkbox 4")
+        cb.place(x=50, y=150)
+
+if __name__ == '__main__':
+    root = Tk()
+    app = Example(root)
+    root.mainloop()
