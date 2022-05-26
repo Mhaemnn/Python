c85eb649f1d3a125b99e039bfa8bebbb8423125b
Mhaemnn <ismuhaimin1@gmail.com>
2022-05-26 19:20:34 +0700

    modifiakasi

--- #59 Python_OOP Encapsulasi/main1.py
+++ #59 Python_OOP Encapsulasi/main1.py
@@ -1,9 +1,20 @@
 class Mobil:
-  def __init__(self,merk):
-    self.__merk = merk
 
-  def tampilkan_merk(self):
-    print(f"Merk: {self.__merk}")
+  def __init__(self,tahun):
+    self.tahun = tahun
 
-jip = Mobil("Jeep")
-jip.tampilkan_merk()
+    @property
+    def tahun(self):
+      return self.__tahun
+
+
+    @tahun.setter
+    def tahun(self,tahun):
+      if tahun > 2021:
+        self.__tahun = 2021
+      elif tahun < 1990:
+        self.__tahun = 1990
+      else:
+        self.__tahun = tahun
+sedan = Mobil(2200)
+print(f"Mobil ini dibuat tahun {sedan.tahun}")
